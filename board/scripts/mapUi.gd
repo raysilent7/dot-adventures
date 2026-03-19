8932168c62a5aeb0fec101ee6be66e5845d006eb
raysilent7 <53231079+raysilent7@users.noreply.github.com>
2026-03-19 18:03:29 -0300

    adiçoes pontuais do mapa
    - adicionando icone do jogador no mapa
     - adicionando uma legenda para facilitar entendimento do mapa

--- board/scripts/mapUi.gd
+++ board/scripts/mapUi.gd
@@ -62,6 +62,18 @@ func updateMap(seenTiles, tileSprites):
 			pos.x - min_x,
 			pos.y - min_y
 		)
-
 		tile.position = normalized * tileSize
+		
+		var board = get_tree().current_scene
+		var player_tile = board.player.currentTile
+		var normalized_player = Vector2(
+			player_tile.x - min_x,
+			player_tile.y - min_y
+		)
+		var player_icon = ColorRect.new()
+		player_icon.color = Color.GREEN
+		player_icon.size = Vector2(tileSize, tileSize)
+		player_icon.position = normalized_player * tileSize
+
+		mapContainer.add_child(player_icon)
 		mapContainer.add_child(tile)
